0d61e8f4680e2be6c2d9c85133618576839c5998
fengyenchia <yanjia20050504@gmail.com>
2025-12-09 23:48:08 +0800

    更新

--- public/01/01.js
+++ public/01/01.js
@@ -5,18 +5,23 @@ let sound = {};
 let video = {};
 
 function preload() {
-    // img.a = loadImage("");
-    
+    img.coffee = loadImage("../assets/images/動畫1-1_第一楨.png");
+    img.paper = loadImage("../assets/images/1-3.png");
+
     // soundFormats('mp3');
     // sound.a = loadSound("");;
 
     video.coffee = createVideo("../assets/videos/動畫1-1.mp4");
+    video.coffee.hide();
+    video.coffee.pause();
 }
+
+
+let startVideo = false;
+let fiveSecond = false;
 
 function setup() {
     createCanvas(1440, 1024);
-    video.coffee.position(0, 0);
-    // video.coffee.position((width - video.coffee.width) / 2, (height - video.coffee.height) / 2);
 }
 
 function draw() {
@@ -33,11 +38,41 @@ function draw() {
 // 將遊戲內容放在gameContent()裡
 function gameContent() {
 
-    if(mouseIsPressed){
-        video.coffee.play();
+    imageMode(CENTER);
+    image(img.coffee, width / 2, height / 2, 1440, 1024);
+
+    fill(0, 120);
+    rect(0, 0, width, height);
+
+    imageMode(CENTER);
+    image(img.paper, width / 2, height / 2);
+
+    if (keyIsPressed && key == ' ') {
+        if (!startVideo && fadeStatus == "none") {
+            video.coffee.loop();
+            startVideo = true;
+        }
     }
 
-    if (mouseIsPressed && fadeStatus == "none") {
+    // 繪製影片、檢查暫停的時間
+    if (startVideo || fiveSecond) {
+        imageMode(CORNER);
+        image(video.coffee, 0, 0, width, height);
+
+        if (video.coffee.time() >= 5 && !fiveSecond) {
+            video.coffee.pause();
+            video.coffee.time(5);
+            fiveSecond = true;
+            startVideo = false;
+        }
+    }
+    // if (fiveSecond) {
+    //     fill(0, 120);
+    //     rect(0, 0, width, height);
+    // }
+
+
+    if (mouseIsPressed && fadeStatus == "none" && fiveSecond) {
         fadeStatus = "out";
     }
 }
