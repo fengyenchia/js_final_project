371f0173c6fc6dba8f990e87eb5c78d5ba684c4d
fengyenchia <yanjia20050504@gmail.com>
2025-12-10 02:35:35 +0800

    更新

--- public/01/01.js
+++ public/01/01.js
@@ -7,6 +7,16 @@ let video = {};
 function preload() {
     img.coffee = loadImage("../assets/images/動畫1-1_第一楨.png");
     img.paper = loadImage("../assets/images/1-3.png");
+    img.check_1 = loadImage("../assets/images/1-4.png");
+    img.check_2 = loadImage("../assets/images/1-5.png");
+    img.check_3 = loadImage("../assets/images/1-6.png");
+    img.check_4 = loadImage("../assets/images/1-7.png");
+    img.check_5 = loadImage("../assets/images/1-8.png");
+    img.order_1 = loadImage("../assets/images/1-9.png");
+    img.order_2 = loadImage("../assets/images/1-10.png");
+    img.order_3 = loadImage("../assets/images/1-11.png");
+    img.order_4 = loadImage("../assets/images/1-12.png");
+    img.order_5 = loadImage("../assets/images/1-13.png");
 
     // soundFormats('mp3');
     // sound.a = loadSound("");
@@ -20,6 +30,9 @@ function preload() {
 let startVideo = false;
 let fiveSecond = false;
 let addCoffee = 0;
+let completeAddCoffee = false;
+let posMoveLeft = 0;
+let moveLeftSpeed = 0.02;
 
 function setup() {
     createCanvas(1440, 1024);
@@ -38,7 +51,7 @@ function draw() {
 // -----------------------------------------
 // 將遊戲內容放在gameContent()裡
 function gameContent() {
-    if (addCoffee < 1) {
+    if (addCoffee == 0 && !(keyIsPressed && key == ' ')) {
         imageMode(CENTER);
         image(img.coffee, width / 2, height / 2, 1440, 1024);
 
@@ -53,7 +66,7 @@ function gameContent() {
         imageMode(CENTER);
         image(img.coffee, width / 2, height / 2, 1440, 1024);
 
-        if (!startVideo && fadeStatus == "none" && addCoffee != 5) {
+        if (!startVideo && fadeStatus == "none" && addCoffee < 5) {
             video.coffee.time(0);
             video.coffee.loop();
             startVideo = true;
@@ -72,13 +85,49 @@ function gameContent() {
             fiveSecond = true;
             startVideo = false;
             addCoffee++;
+            console.log(addCoffee);
         }
     }
 
-    // if (fiveSecond) {
-    //     fill(0, 120);
-    //     rect(0, 0, width, height);
-    // }
+    if ((addCoffee != 0 || startVideo) && addCoffee != -1) {
+        image(img.order_1, 50, (height - height/5) * 1 / 5);
+        image(img.order_2, 50, (height - height/5) * 2 / 5);
+        image(img.order_3, 50, (height - height/5) * 3 / 5);
+        image(img.order_4, 50, (height - height/5) * 4 / 5);
+        image(img.order_5, 50, (height - height/5) * 5 / 5);
+        if(addCoffee >= 1){
+            image(img.check_1, 260, (height - height/5) * 1 / 5);
+        }
+        if(addCoffee >=2){
+            image(img.check_2, 260, (height - height/5) * 2 / 5);
+        }
+        if(addCoffee >=3){
+            image(img.check_3, 260, (height - height/5) * 3 / 5);
+        }
+        if(addCoffee >=4){
+            image(img.check_4, 260, (height - height/5) * 4 / 5);
+        }
+        if(addCoffee >=5){
+            image(img.check_5, 260, (height - height/5) * 5 / 5);
+            addCoffee = -1;
+            completeAddCoffee = true;
+        }        
+    }
+    if(addCoffee == -1 && completeAddCoffee){
+       let posX_check = lerp(260, -10, posMoveLeft);
+       let posX_order = lerp(50, -220, posMoveLeft);
+         posMoveLeft += moveLeftSpeed;
+       image(img.order_1, posX_order, (height - height/5) * 1 / 5);
+        image(img.order_2, posX_order, (height - height/5) * 2 / 5);
+        image(img.order_3, posX_order, (height - height/5) * 3 / 5);
+        image(img.order_4, posX_order, (height - height/5) * 4 / 5);
+        image(img.order_5, posX_order, (height - height/5) * 5 / 5);
+       image(img.check_1, posX_check, (height - height/5) * 1 / 5)
+       image(img.check_2, posX_check, (height - height/5) * 2 / 5)
+       image(img.check_3, posX_check, (height - height/5) * 3 / 5)
+       image(img.check_4, posX_check, (height - height/5) * 4 / 5)
+       image(img.check_5, posX_check, (height - height/5) * 5 / 5)
+    }
 
 
     if (mouseIsPressed && fadeStatus == "none" && fiveSecond && addCoffee == 5) {
